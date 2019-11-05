d97966003b501453f598731ab38d1f188bf6c2bd
RaziyeChamanpara <r.chamanpara@gmail.com>
2019-11-05 14:50:18 +1100

    add UnitTest for DataAccess

--- VendingMachie.DataAccess.Tests/MoneyRepositoryTests.cs
+++ VendingMachie.DataAccess.Tests/MoneyRepositoryTests.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachie.Models;
+using FluentAssertions;
 
 namespace VendingMachie.DataAccess.Tests
 {
@@ -6,8 +8,47 @@ namespace VendingMachie.DataAccess.Tests
   public class MoneyRepositoryTests
   {
     [TestMethod]
-    public void TestMethod1()
+    public void ResetCredit_Always_ShouldSetCreditToZero()
     {
+      //Arrange
+      MoneyRepository moneyRepository = new MoneyRepository();
+
+      //Act
+      moneyRepository.ResetCredit();
+
+      //Asert
+      var credit = moneyRepository.GetAvailableCredit();
+      Assert.AreEqual(credit, 0);
+    }
+
+    [TestMethod]
+    public void ResetCash_Always_ShouldSetCashToZero()
+    {
+      //Arrange
+      MoneyRepository moneyRepository = new MoneyRepository();
+
+      //Act
+      moneyRepository.ResetCash();
+
+      //Assert
+      var cash=moneyRepository.GetAvailableCash();
+      Assert.AreEqual(cash, 0);
+    }
+
+    [TestMethod]
+    public void GetAvailableCredit_Always_ShouldReturnCredit()
+    {
+      //Arrange
+      MoneyRepository moneyRepository = new MoneyRepository();
+
+      //Act
+      moneyRepository.Add(4.5m, PaymentMethod.Credit);
+      moneyRepository.Add(4.5m, PaymentMethod.Credit);
+      moneyRepository.Add(4.5m, PaymentMethod.Cash);
+
+      //Assert
+      var credit=moneyRepository.GetAvailableCredit();
+      credit.Should().Be(9m);
     }
   }
 }
